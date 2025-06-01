3c5a56ecfb203ddccc6b8caba89aa9e673439a7a
clc16328 <lche0501@uni.sydney.edu.au>
2025-06-01 20:13:43 +1000

    minor change

--- api/owners.php
+++ api/owners.php
@@ -1,5 +1,5 @@
 <?php
-$SUPABASE_URL = "https://vmueeybtjxkmumzftldv.supabase.co";
+$SUPABASE_URL = "https://vmueeybtjxkqumzftldv.supabase.co";
 $SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtdWVleWJ0anhrbXVtemZ0bGR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2ODg2MzMsImV4cCI6MjA2NDI2NDYzM30.g6TLb1Z2cOjghSxYs2GIGUxL7cwqGFNY2vVIO0tn2QI";
 
 $url = "$SUPABASE_URL/rest/v1/owners";
